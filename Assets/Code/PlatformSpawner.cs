b2520367364c6cb736b64a2ce69b1afeffe7019a
Alafresh <51685896+Alafresh@users.noreply.github.com>
2023-05-23 07:40:14 -0500

    update

--- Assets/Code/PlatformSpawner.cs
+++ Assets/Code/PlatformSpawner.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformSpawner : MonoBehaviour
 {
+    public GameObject platformPrefab;
+    public Transform lastPlatform;
+    Vector3 lastPosition;
+    Vector3 newPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        lastPosition = lastPlatform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void GeneratePosition()
+    {
+        
+        newPos = lastPosition;
+
+        int rand = Random.Range(0, 2);
+
+        if (rand > 0)
+        {
+            newPos.x += 2f;
+        }
+        else
+        {
+            newPos.z += 2f;
+        }
     }
 }
