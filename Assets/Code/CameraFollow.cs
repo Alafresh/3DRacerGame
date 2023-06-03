a8398f8db22289a460a042e4c6ba28e09ec3821a
Alafresh <51685896+Alafresh@users.noreply.github.com>
2023-06-03 11:48:57 -0500

    Update Platform spawning and camera following

---
+++ Assets/Code/CameraFollow.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    Vector3 distance;
+    [SerializeField] float smoothSpeed = 0.125f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = target.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Follow();
+    }
+
+    void Follow() {
+        
+        Vector3 currentPos = transform.position;
+        Vector3 targetPos = target.position - distance;
+
+        transform.position = Vector3.Lerp(currentPos, targetPos, smoothSpeed * Time.deltaTime);
+    }
+}
--- Assets/Code/PlatformSpawner.cs
+++ Assets/Code/PlatformSpawner.cs
@@ -1,21 +1,36 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlatformSpawner : MonoBehaviour
 {
-    public GameObject platformPrefab;
-    public Transform lastPlatform;
+    [SerializeField] GameObject platformPrefab;
+    [SerializeField] Transform lastPlatform;
     Vector3 lastPosition;
     Vector3 newPos;
+
+    bool stop;
     // Start is called before the first frame update
     void Start()
     {
         lastPosition = lastPlatform.position;
+        StartCoroutine(SpawnPlatforms());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+
+    IEnumerator SpawnPlatforms()
+    {
+        while(!stop)
+        {
+            GeneratePosition();
+            Instantiate(platformPrefab, newPos, Quaternion.identity);
+            lastPosition = newPos;
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 
     void GeneratePosition()
